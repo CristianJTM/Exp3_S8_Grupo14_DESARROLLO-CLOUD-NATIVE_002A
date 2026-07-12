c1353b4e5c79067264e0a2b2452dd75c31b91e6b
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-07-12 19:42:20 -0400

    Add RabbitMQ consumers for Guia and Transportista message processing

---
+++ transportmanagement-consumer/src/main/java/com/duoc/transportmanagement/listener/GuiaConsumer.java
@@ -0,0 +1,44 @@
+package com.duoc.transportmanagement.listener;
+
+import com.duoc.transportmanagement.dto.GuiaMessageDTO;
+import com.duoc.transportmanagement.repository.S3Repository;
+import com.duoc.transportmanagement.service.GuiaService;
+import com.duoc.transportmanagement.util.RabbitConstants;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+public class GuiaConsumer {
+
+    @Autowired
+    private GuiaService guiaService;
+
+    @RabbitListener(queues = RabbitConstants.GUIA_QUEUE)
+    public void receive(GuiaMessageDTO dto){
+
+        switch(dto.getOperacion()){
+
+            case "CREATE":
+                guiaService.createGuia(dto.getGuiaCreate());
+                break;
+
+            case "UPDATE":
+                guiaService.updateGuia(dto.getId(), dto.getGuiaUpdate());
+                break;
+
+            case "DELETE":
+                guiaService.deleteGuia(dto.getId());
+                break;
+
+            case "UPLOAD_S3":
+                guiaService.subirArchivoS3(dto.getId());
+                break;
+
+            case "DELETE_S3":
+                guiaService.eliminarArchivoS3(dto.getId());
+                break;
+        }
+
+    }
+}
---
+++ transportmanagement-consumer/src/main/java/com/duoc/transportmanagement/listener/TransportistaConsumer.java
@@ -0,0 +1,36 @@
+package com.duoc.transportmanagement.listener;
+
+import com.duoc.transportmanagement.dto.TransportistaMessageDTO;
+import com.duoc.transportmanagement.service.TransportistaService;
+import com.duoc.transportmanagement.util.RabbitConstants;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransportistaConsumer {
+
+    @Autowired
+    private TransportistaService transportistaService;
+
+    @RabbitListener(queues = RabbitConstants.TRANSPORTISTA_QUEUE)
+    public void receive(TransportistaMessageDTO dto){
+
+        switch(dto.getOperacion()){
+
+            case "CREATE":
+                transportistaService.saveTransportista(dto.getTransportistaDTO());
+                break;
+
+            case "UPDATE":
+                transportistaService.updateTransportista(dto.getId(),
+                        dto.getTransportistaDTO());
+                break;
+
+            case "DELETE":
+                transportistaService.deleteTransportista(dto.getId());
+                break;
+        }
+
+    }
+}
